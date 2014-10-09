ecd5e50a98b5f0635ba74ab7c1e4a83df1039c11
Tom Ashworth <tashworth@twitter.com>
2014-10-09 12:06:27 +0100

    Clean up assoc

--- test/assoc_test.js
+++ test/assoc_test.js
@@ -2,11 +2,18 @@ var expect = require('expect.js');
 var Rash = require('../index');
 
 describe('Rash#assoc', function() {
-  it('returns the two len array', function() {
+  it('returns k-v pair', function() {
     var h = {
       'letters': ['a', 'b', 'c']
     };
     expect(Rash(h).assoc('letters')).to.eql(['letters', ['a', 'b', 'c']]);
+  });
+
+  it('returns k-v pair if falsey value', function() {
+    var h = {
+      counter: 0
+    };
+    expect(Rash(h).assoc('counter')).to.eql(['counter', 0]);
   });
 
   it('returns undefined if no key', function() {
